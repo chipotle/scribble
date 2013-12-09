15d308d01d5768cefbb441232ae0d315179d996b
Watts Martin <layotl@gmail.com>
2013-12-09 09:55:06 -0800

    Start template directory

--- scribble/views.py
+++ scribble/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 
 from scribble import app
+# from models import *
+
 
 @app.route('/')
 def index():
     """A none-too-useful index page."""
-    return render_template('index.html')
+    return render_template('index.html', name='Scribble')
