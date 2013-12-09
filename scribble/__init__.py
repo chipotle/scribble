792317ffa1016743937666c919b4b678e4aab1a5
Watts Martin <layotl@gmail.com>
2013-12-09 09:53:06 -0800

    Add default settings file

--- scribble/__init__.py
+++ scribble/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# load default settings file
+app.config.from_object('scribble.default_settings')
+
+# override settings with file pointed to by SCRIBBLE_CONFIG
+app.config.from_envvar('SCRIBBLE_CONFIG', silent=True)
+
+# noinspection PyUnresolvedReferences
 import scribble.views
---
+++ scribble/default_settings.py
@@ -0,0 +1,15 @@
+### SETTINGS ###
+
+# Values may be overridden by a settings file pointed to with the environment
+# variable SCRIBBLE_CONFIG. Its value must be the *full, absolute* path to
+# the config file.
+
+# Database configuration
+
+RDB_HOST = 'localhost'
+RDB_PORT = 28015
+RDB_DB   = 'scribble'
+
+# Encryption key
+
+SECRET_KEY = '28pY5zO3AaV1HsX3udQgeq032l1JsvBs'
