6f099db71a4cd0de2287bcab6345abf8430bdc45
Watts Martin <layotl@gmail.com>
2013-12-09 18:31:48 -0800

    Add register view

--- scribble/views.py
+++ scribble/views.py
@@ -1,10 +1,21 @@
-from flask import render_template
+from flask import render_template, request, redirect
 
 from scribble import app
-# from models import *
-
+from models import *
 
 @app.route('/')
 def index():
     """A none-too-useful index page."""
     return render_template('index.html', name='Scribble')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register a new user."""
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        ok = User.create(request.form['username'],
+                         request.form['password'])
+        if ok:
+            return redirect('/')
+        return render_template('register.html', error=True)
