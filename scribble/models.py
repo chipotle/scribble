7a0c96bb55d70cbc35f6106d4c7e35b07746cc8a
Watts Martin <layotl@gmail.com>
2013-12-09 18:31:17 -0800

    Add password methods on User model

--- scribble/models.py
+++ scribble/models.py
@@ -1,5 +1,20 @@
 from rdb_model import *
+from scribble import bcrypt
 
 
 class User(Model):
     _table = 'users'
+
+    def __setattr__(self, key, value):
+        """Store hash of password instead of text."""
+        if key == 'password':
+            value = bcrypt.generate_password_hash(value)
+        super(User, self).__setattr__(key, value)
+
+    def check_password(self, password):
+        """Check password against hash, returning true on match."""
+        return bcrypt.check_password_hash(self.password, password)
+
+
+class Item(Model):
+    _table = 'items'
